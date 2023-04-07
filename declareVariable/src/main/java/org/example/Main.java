5a2fb4d236a351cbdae346b340d1e37741cb172a
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-04-07 11:07:27 +0700

    khai báo biến

---
+++ declareVariable/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i = 10;
+        System.out.println("số nguyên:"+" "+i);
+        float f = 20.5f;
+        System.out.println("số thực:"+" "+f);
+        double d= 20.5;
+        System.out.println("số thực lớn:"+" "+d);
+        boolean b = true;
+        System.out.println("kiểu chuỗi:"+" "+b);
+        char c = 'a';
+        System.out.println("kiểu ký tự:"+" "+c);
+        String s = "Hà Nội";
+        System.out.println("kiểu chuỗi:"+" "+s);
+    }
+}

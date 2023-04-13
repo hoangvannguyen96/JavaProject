0cae9eb405fd98e656208513397fc60a3c6a92bd
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-04-13 22:04:08 +0700

    đếm các phần tử trong mảng có chữ số tận cùng bằng 5

--- 20baitap/countTheLastDigit5/src/main/java/org/example/Main.java
+++ 20baitap/countTheLastDigit5/src/main/java/org/example/Main.java
@@ -2,13 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int arr []={1,25,34,5,127,135,23,55,46,80};
+        int arr []={1,24,34,127,134,23,54,46,80};
         int count=0;
+        boolean check=false;
         for(int i=0;i<arr.length;i++){
             if(arr[i]%10==5){
                 count++;
+                check=true;
             }
         }
-        System.out.printf("có %d số trong mảng có chữ số tận cùng bằng 5",count);
+        if(check){
+            System.out.printf("có %d số trong mảng có chữ số tận cùng bằng 5",count);
+        }else {
+            System.out.println("không có phần tử nào trong mảng có chữ số tận cùng bằng 5");
+        }
     }
 }

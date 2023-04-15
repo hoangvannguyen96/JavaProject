800766a0893116b4cbcb30df39fe00c9b06fcbe4
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-04-15 16:02:36 +0700

    sắp xếp mảng phần tử dương tăng dần và phần tử âm giữ nguyên vị trí

---
+++ 20baitap/positiveNumbersInIncreasing/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {1, 5, -3, 4, 2, -8, 0, -4};
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] >= 0 && arr[j] >= 0 && arr[i] < arr[j]) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                }
+            }
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+}

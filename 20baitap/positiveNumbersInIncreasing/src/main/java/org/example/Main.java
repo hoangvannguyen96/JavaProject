209aaab3bbbcb64d72d9deb44883f73eca6fa48b
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-04-15 16:28:32 +0700

    sắp xếp dương tăng dần, âm giữ nguyên vị trí

--- 20baitap/positiveNumbersInIncreasing/src/main/java/org/example/Main.java
+++ 20baitap/positiveNumbersInIncreasing/src/main/java/org/example/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         int arr[] = {1, 5, -3, 4, 2, -8, 0, -4};
         for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] >= 0 && arr[j] >= 0 && arr[i] < arr[j]) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] >= 0 && arr[j] >= 0 && arr[i] > arr[j]) {
                         int temp = arr[i];
                         arr[i] = arr[j];
                         arr[j] = temp;

0a7855e09d79286cf9527d10464af1a729c781e3
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-04-15 21:19:38 +0700

    xóa tất cả các số âm trong mảng

---
+++ 20baitap/deleteAllNegativeInArray/src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 7, -7, 34, -6, -1, 12};
+        System.out.println(Arrays.toString(deleteAllNegativeInArray(arr)));
+    }
+
+    public static int[] deleteAllNegativeInArray(int arr[]) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                count++;
+            }
+        }
+        int brr[] = new int[arr.length - count];
+        for (int i = 0, j = 0; i < arr.length; i++) {
+            if (arr[i] >= 0) {
+                brr[j] = arr[i];
+                j++;
+            }
+        }
+        int crr[] = new int[brr.length];
+        for (int i = 0; i < brr.length; i++) {
+            crr[i] = brr[i];
+        }
+        return crr;
+    }
+}

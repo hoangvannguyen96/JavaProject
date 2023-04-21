ee83f7ca95380549e5427450527ffb3b11914ad6
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-04-21 08:37:14 +0700

    thực hành test máy tính đơn giản

---
+++ AutomatedTestingAndTDD/SimpleCalculator/src/main/java/org/example/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+//        throw new UnsupportedOperationException();
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+//        throw new UnsupportedOperationException();
+        return first - second;
+    }
+}

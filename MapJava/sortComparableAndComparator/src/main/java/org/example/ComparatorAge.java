27f53ecad5ab9a6f505e823f286422701fd49932
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-04-28 11:16:48 +0700

    [Thực hành] Sắp xếp với Comparable và Comparator

---
+++ MapJava/sortComparableAndComparator/src/main/java/org/example/ComparatorAge.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class ComparatorAge implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.getAge().compareTo(o2.getAge());
+    }
+}
+
